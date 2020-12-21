98bc9ece0a02f828f327bc25e46f7ccf641f97db
yeungi <ian.yeung88@gmail.com>
2020-12-21 06:23:42 -0500

    Added account class

---
+++ model/accounts.php
@@ -0,0 +1,36 @@
+<?php
+    class Account{
+        private $id;
+        private $email;
+        private $fname;
+        private $lname;
+        private $birthday;
+        private $password;
+
+        public function __construct($email, $fname, $lname, $birthday, $password){
+            $this->email = $email;
+            $this->fname = $fname;
+            $this->lname = $lname;
+            $this->birthday = $birthday;
+            $this->password = $password;
+        }
+        public function getID(){
+            return $this->id;
+        }
+        public function getEmail(){
+            return $this->email;
+        }
+        public function getFirstName(){
+            return $this->fname;
+        }
+        public function getLastName(){
+            return $this->lname;
+        }
+        public function getBirthday(){
+            return $this->birthday;
+        }
+        public function getPassword(){
+            return $this->password;
+        }
+    }
+?>
